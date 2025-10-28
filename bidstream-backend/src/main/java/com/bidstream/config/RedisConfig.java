df75458f4ee9bb89871b67cb393cc21e9a0bd38c
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-10-28 12:00:00 +0530

    feat: configure Redis connection factory

---
+++ bidstream-backend/src/main/java/com/bidstream/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.bidstream.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6379}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+}
