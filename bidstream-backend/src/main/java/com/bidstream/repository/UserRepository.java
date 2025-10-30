a9a6d5e1e7fc38e97671839ad782f492fa3fc784
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-10-30 12:00:00 +0530

    feat: create user repository for persistence

---
+++ bidstream-backend/src/main/java/com/bidstream/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.bidstream.repository;
+
+import com.bidstream.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
