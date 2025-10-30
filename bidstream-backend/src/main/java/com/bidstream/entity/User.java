bdb206fa9453147ae0e777c5746e2ec375785ccf
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-10-30 10:00:00 +0530

    feat: create user entity for application identity

---
+++ bidstream-backend/src/main/java/com/bidstream/entity/User.java
@@ -0,0 +1,41 @@
+package com.bidstream.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
