92febab8c5fbbe9a207ab0793e5e4304f350e8e5
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-10-31 14:00:00 +0530

    feat: implement user registration endpoint

---
+++ bidstream-backend/src/main/java/com/bidstream/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.bidstream.controller;
+
+import com.bidstream.dto.UserRegistrationDto;
+import com.bidstream.entity.User;
+import com.bidstream.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody UserRegistrationDto registrationDto) {
+        User user = userService.registerUser(registrationDto);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+}
