730daaed0ed34636be2c96827cf485155df47b04
Imtiyaz Ahamad <imtiyazahamad703@gmail.com>
2025-11-30 12:00:00 +0530

    feat: create bid repository for persistence

---
+++ bidstream-backend/src/main/java/com/bidstream/repository/jpa/BidRepository.java
@@ -0,0 +1,21 @@
+package com.bidstream.repository.jpa;
+
+import com.bidstream.entity.Bid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BidRepository extends JpaRepository<Bid, Long> {
+    
+    List<Bid> findByAuctionIdOrderByCreatedAtDesc(Long auctionId);
+    
+    Page<Bid> findByAuctionId(Long auctionId, Pageable pageable);
+    
+    Page<Bid> findByBidderEmail(String bidderEmail, Pageable pageable);
+    
+    Bid findTopByAuctionIdOrderByAmountDesc(Long auctionId);
+}
